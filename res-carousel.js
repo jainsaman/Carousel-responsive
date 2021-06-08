0ce2838d803b943eb41529b26ed731dc3f3b8d68
Saman Jain <81140577+jainsaman@users.noreply.github.com>
2021-06-08 21:25:05 +0530

    Update res-carousel.js

--- res-carousel.js
+++ res-carousel.js
@@ -1,39 +1,24 @@
-const container = document.querySelector('.container');
-const slide = document.querySelector('.slide');
-const box = document.querySelectorAll('.box');
+const slides = document.getElementsByClassName('box');
 
-let width = box[0].offsetWidth + 30;
-slide.style.minWidth = `${box.length * width}px`;
+let counter = 1;
 
-let start;
-let change;
+slider(counter);
 
-container.addEventListener('dragstart' , (e)=>{
-    start = e.clientX;
-});
-container.addEventListener('dragover' , (e)=>{
-    e.preventDefault();
-    let touch = e.clientX;
-    change = start - touch;
-});
-
-container.addEventListener('dragend' , slideshow);
-
-//touch event for mobiles
-container.addEventListener('touchstart' , (e)=>{
-    start = e.touches[0].clientX;
-});
-container.addEventListener('touchmove' , (e)=>{
-    e.preventDefault();
-    let touch = e.touches[0];
-    change = start - touch.clientX;
-});
-container.addEventListener('touchend' , slideshow);
-
-function slideshow(){
-    if(change > 0){
-        container.scrollLeft += width;
-    } else {
-        container.scrollLeft -= width;
+function plusslide(n){
+    slider(counter += n);
+    console.log(slides.length);
+}
+function slider(n){
+    let i;
+    
+    if(n > slides.length){
+        counter = 1;
     }
-};
+    if(n < 1){
+        counter = slides.length;
+    }
+    for(i=0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[counter - 1].style.display = "block";
+}
